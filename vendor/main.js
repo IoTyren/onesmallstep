81dec4342c1fb3e1fc0d6b053787fb2b752c40c2
Leon Johnson <SlurtoGames@gmail.com>
2022-01-29 05:10:28 -0500

    Close

--- vendor/main.js
+++ vendor/main.js
@@ -1,1 +1,17 @@
-alert("Hellooooo");
+const myGuess = Math.floor(Math.random() * 20) + 1;
+let guesses = 0;
+let guess;
+
+while (guess !==myGuess) {
+    guess = parseInt(prompt("What Number Am I Thinking of ?"), 10);
+    guesses++;
+    if (guess < myGuess) {
+        alert("Higher!")
+    
+    } else if (guess > myGuess) {
+            alert("Lower !");
+
+        
+    }
+}
+alert (`YOU WIN! You got it in ${guesses} trys! BUM `);
